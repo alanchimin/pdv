38633adff472b4cd75991fbf8f6a947f9b729d6c
Alan Chimin <alan.chimin@hipe.cc>
2025-07-09 11:46:32 -0300

    - adicionando pt br

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -6,5 +6,6 @@
 <script src="/js/jquery.min.js"></script>
 <script src="/js/bootstrap.bundle.min.js"></script>
 <script src="/js/bootstrap-select.min.js"></script>
+<script src="/js/bootstrap-select-pt_BR.min.js"></script>
 </body>
 </html>
